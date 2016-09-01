3b73c0c7f2a00f04a4d541750dbc28b213ae0e1d
Jason Yang <jasonyang96@ucla.edu>
2016-08-31 19:00:45 -0700

    got flask_login to work

--- acaspot.py
+++ acaspot.py
@@ -1,32 +1,32 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from models import Users
 import flask_login
 app = Flask(__name__)
+app.secret_key = 'acaspot'
 login_manager = flask_login.LoginManager()
 login_manager.init_app(app)
 
-users = Users()
+users = {}
 
 # flask-login functions
 
 @login_manager.user_loader
 def user_loader(user):
-    if not users.user_exists(user):
+    if user not in users:
         return None
 
-    return users.get_user(user)
+    return users[user]
 
 @login_manager.request_loader
 def request_loader(request):
     user = request.form.get('user')
-    if not users.user_exists(user):
+    if user not in users:
         return None
 
     #TODO hash pw
-    auth = request.form['pw'] == users.get_user(user)['pw']
-    users.authenticate_user(user, auth)
+    users[user].authenticated = request.form['pw'] == users[user]['pw']
 
-    return users.get_user(user)
+    return users[user]
 
 @app.route('/')
 def index():
@@ -37,22 +37,28 @@ def login():
     if request.method == 'GET':
         return render_template('login.html')
 
-    user = request.form['user']
-    if request.form['pw'] == users.get_user(user)['pw']: 
-        #flask_login.login_user(users.get_user(user))
-        return redirect(url_for('profile', user=users.get_user(user)))
+    # check if user exists
+    user = str(request.form['user'])
+    print users
 
-    flash('Bad login')
+    if user not in users:
+        flash('User does not exist')
+        return 'User does not exist'
+
+    if request.form['pw'] == users[user].pw: 
+        flask_login.login_user(users[user])
+        return redirect(url_for('profile', user=users[user]))
+
+    flash('Incorrect Password')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'GET':
         return render_template('register.html')
 
-    user = request.form['user']
-    if (users.user_exists(user)):
-        flash('User already exists')
-        return
+    user = str(request.form['user'])
+    if user in users:
+        return 'User already exists'
 
     #TODO hash pw
     pw = request.form['pw']
@@ -66,17 +72,18 @@ def register():
     major = request.form['major']
     time_commit = request.form['time_commit']
 
-    users.add_user(user, pw, name, email, number, voice_part, sing_exp, music_exp, year, major, time_commit)
-    #flask_login.login_user(users.get_user(user))
-    return redirect(url_for('profile', user=users.get_user(user)))
+    users[user] = Users(user, pw, name, email, number, voice_part, sing_exp, music_exp, year, major, time_commit)
+    flask_login.login_user(users[user])
+    return render_template('profile.html', user=users[user])
+
 @flask_login.login_required
 @app.route('/profile', methods=['GET', 'POST'])
 def profile():
     if request.method == 'GET':
-        return render_template('profile.html', user=users[current_user.user])
+        return render_template('profile.html', user=current_user)
 
     #TODO edit profile fields
-    pass
+    return render_template('profile.html', user=users[user])
 
 @app.route('/logout')
 @flask_login.login_required
--- models.py
+++ models.py
@@ -1,45 +1,34 @@
 import flask_login
 
 class Users(flask_login.UserMixin):
-    users = {}
+    def __init__(self, user, pw, name, email, number, voice_part, sing_exp, music_exp, year, major, time_commit):
+        self.user = user
+        self.pw = pw
+        self.name = name
+        self.email = email
+        self.number = number
+        self.voice_part = voice_part
+        self.sing_exp = sing_exp
+        self.music_exp = music_exp
+        self.year = year
+        self.major = major
+        self.time_commit = time_commit
+        self.authenticated = False
 
-    def __init__(self):
-        pass
+    def is_active(self):
+        return True
 
-    def add_user(self, user, pw, name, email, number, voice_part, sing_exp, music_exp, year, major, time_commit):
-        if user in self.users:
-            return -1
+    def get_id(self):
+        return self.user
 
-        self.users[user] = {
-            "pw": pw,
-            "name": name,
-            "email": email,
-            "number": number,
-            "voice_part": voice_part,
-            "sing_exp": sing_exp,
-            "music_exp": music_exp,
-            "year": year,
-            "major": major,
-            "time_commit": time_commit,
-            "is_active": True,
-            "is_anonymous": False,
-            "is_authenticated": False
-        }
-        print self.users[user]["is_active"]
-        return self.users[user]
+    def is_authenticated(self):
+        return self.authenticated
 
-    def list_users(self):
-        for k, v in self.users.items():
-            print(k, v)
+    def is_anonymous(self):
+        return False
 
-    def user_exists(self, user):
-        return user in self.users
-
-    def authenticate_user(user, auth):
-        self.users[user]["auth"] = auth
-
-    def get_user(self, user):
-        return self.users[user]
+    def authenticate_user(self, auth):
+        self.authenticated = auth
 
 class Groups:
     groups = {}
